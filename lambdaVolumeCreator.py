d5e12a616e29402bd26fa10e707d657c5528de5b
Cole <31732303+nickj10@users.noreply.github.com>
2018-01-10 17:09:55 +0000

    Finish attaching newly created volumes

--- lambdaVolumeCreator.py
+++ lambdaVolumeCreator.py
@@ -10,6 +10,7 @@
 
 
 import boto3
+import time
 
 client = boto3.client('ec2')
 ec2 = boto3.resource('ec2') #defines the connection
@@ -20,6 +21,7 @@ snapshots = client.describe_snapshots(MaxResults=1000, OwnerIds=[myAccount])['Sn
 #images = ec2.images.filter(Owners=["self"])
 
 def lambda_handler(event, context):
+	# 1) Gets the instance ids that have 'Backup' as tag ==> DevAF01a
 	reservations = client.describe_instances(
 		Filters=[
 			{'Name': 'tag-key', 'Values': ['backup', 'Backup']},
@@ -32,76 +34,133 @@ def lambda_handler(event, context):
 			[i for i in r['Instances']]
 			for r in reservations
 		], [])
+		
+	# 2) Gets the instance ids that have 'Target' as tag ==> DevAF01b
+	targets = client.describe_instances(
+		Filters=[
+			{'Name': 'tag-key', 'Values': ['target', 'Target']},
+		]
+		).get(
+			'Reservations', []
+		)
+	target_instances = sum(
+		[
+			[j for j in t['Instances']]
+			for t in targets
+		], [])
 
-	print "Found %d instances that need evaluated" % len(instances)
+	print "Found %d instances that need backed up" % len(instances)
+	print "Found %d target instances" % len(target_instances)
 
-	#empty list of volumes to be detached
-	toBeDetached = []
+	#empty lists
+	attachedToDev = []
 	foundInDesc = []
+	newVolumes =[]
+	devices = []
 	volumesExist = False
 
-	for instance in instances:
-		
+	# 4) Find volumes attached to DevAF01b so that they can be detached later on
+	for instance in target_instances:
 		for v in volumes:
 			v.load() # loads attachments for each volume
 			try:
+				#find the volumes attached to DevAF01b
 				if v.attachments[0]['InstanceId'] == instance['InstanceId']:
-					#print "Volume id: ", v.id
-					#print "Instance: ", instance['InstanceId']
-					#print "Instance id attached: ", v.attachments[0]['InstanceId']
-				
-					# includes the volume to the list of volumes to be detached later on
-					if str(v.id) not in toBeDetached:
-						toBeDetached.insert(0,str(v.id))
+					attachedToDev.insert(0,v)
 			except IndexError:
-				volumesExist = True
+				volumesExist = False
 					
 	
 	print ""
-	print "Volumes to be detached"
+	print "Volumes attached to DevAF01b"
 	print "======================"
-	for det in toBeDetached:
-		print det
+	for att in attachedToDev:
+		print att
 	
 	print ""
 	if volumesExist == True:
 		print "There are volumes that need to be deleted"
 	else:
-		# check if there are any snapshots related to the volumes in toBeDetached
-		for vol in toBeDetached:
+		# check if there are any snapshots related to the instances
+		for i in instances:
 			for snaps in snapshots:
-				if snaps['Description'].find(vol) > 0:
-					print "Volume id", vol,"found in description of",snaps['SnapshotId']
+				# 3) Find snapshots connected to DevAF01a
+				if snaps['Description'].find(i['InstanceId']) > 0:
+					#print "Volume id", vol,"found in description of",snaps['SnapshotId']
+					print "Instance id", i['InstanceId'],"found in description of",snaps['SnapshotId']
 					if snaps['SnapshotId'] not in foundInDesc:
 						foundInDesc.insert(0,str(snaps['SnapshotId']))
 						print snaps['StartTime']
 						
 		print ""
-		print "Snapshots to be used"
+		print "Snapshots related to DevAF01a"
 		print "===================="
-		print ""
 		
 		if not foundInDesc:
 			print "There are no snapshots in the list"
 		else:
 			for j in foundInDesc:
 				print j
-				
-		
-		#create new volumes for each snapshot
+		print ""
+		print "===="
+		print "Creating new volumes"
+		print "===="
+
+		# 5) Create new volumes for each snapshot
 		for s in foundInDesc:
 			newVol = client.create_volume(SnapshotId=s, AvailabilityZone='eu-central-1b')
 			print "Volume created! Volume Id:", newVol['VolumeId']
-			volume = ec2.Volume(newVol['VolumeId'])
-			volume.load()
+			newVolumes.insert(0,str(newVol['VolumeId'])) # we add this to the list of new volumes created
+
+		# wait until the new volumes are available
+		time.sleep(30) #sleep for 30 seconds
+		flag = 0
+		print ""
+		print "===="
+		print "Detaching old volumes"
+		print "===="
+		
+		#  6) Detach old volumes from DevAF01b
+		for a in attachedToDev:
+		 	a.load()
+		 	print "Device name for volume", a.attachments[0]['VolumeId'], ":", a.attachments[0]['Device']
+		 	devices.insert(0,str(a.attachments[0]['Device'])) #adds device to list of device
+ 		 	try:
+		 		resp = a.detach_from_instance(
+		 			Device = a.attachments[0]['Device'],
+		 			Force = False,
+		 			InstanceId = target_instances[0]['InstanceId'],
+		 			DryRun = False
+		 		)
+		 		print "Volume", flag, "successfully detached!"
+		 		flag = flag + 1
+		 	except IndexError:
+		 		print "Volume", flag, "was not detached. Moving on to the next one."
+		 		flag = flag + 1
+		 	
+		# 7) After detaching the volume, we attach the new one created
+		flag2 = len(newVolumes) - 1
+		
+		time.sleep(30) # wait for a while until the volumes are fully detached
+		
+		print ""
+		print "===="
+		print "Attaching new volumes"
+		print "===="
+		
+		for a in newVolumes:
 			try:
-				print "Device of new volume:", volume.attachments[0]['Device']
-			except IndexError:
-				print "Volume does not have a device yet"
-			for vols in toBeDetached:
-		 		#get the device name
-		 		volDet = ec2.Volume(vols)
-		 		volDet.load()
-		 		print volDet.attachments[0]['Device']
-		 	
-		 	
+				n = ec2.Volume(a)
+				print "Device name to be used:", devices[flag2], "with volume", a, "attaching to", target_instances[0]['InstanceId']
+				resp2 = n.attach_to_instance(
+		 			Device = devices[flag2],
+					InstanceId = target_instances[0]['InstanceId'],
+		 			DryRun = False
+		 		)
+				print "New volume is attached to the instance"
+		 		flag2 = flag2 - 1
+		 	except IndexError:
+		 		print "The new volume", a, "was not attached to the instance."
+		 		flag2 = flag2 - 1
+			
+		 
